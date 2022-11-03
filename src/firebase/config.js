b1a5f7d742db78bf66d599bfdbaecde1809ebd3b
geostojanov <gestojanov@gmail.com>
2022-11-03 09:48:39 +0100

    Lesson 102 - firebase setup - Done

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQkVu6XI5VpUw3dGzerwYeXs64KHJ1wKM",
+  authDomain: "netninja-vue-sites.firebaseapp.com",
+  projectId: "netninja-vue-sites",
+  storageBucket: "netninja-vue-sites.appspot.com",
+  messagingSenderId: "114244171265",
+  appId: "1:114244171265:web:35e1ab69e220930bc63200"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+export { projectFirestore, timestamp }
