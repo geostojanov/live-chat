4ddf4e2bba6a36d92098408a9f771a33216f3d5b
geostojanov <gestojanov@gmail.com>
2022-11-04 10:17:25 +0100

    Lesson 104 - switching between forms - Done

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,17 +1,33 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
-    <SignupForm/>
-    <!-- <LoginForm/> -->
+    <div v-if="showLogin">
+      <h2>Login</h2>
+      <LoginForm/>
+      <p>No account yet? <span @click="showLogin = false">Signup</span></p>
+    </div>
+    <div v-else>
+      <h2>Sign up</h2>
+      <SignupForm/>
+      <p>Already have an account? <span @click="showLogin = true">Login</span></p>
+    </div>
   </div>
 </template>
 
 <script>
 import SignupForm from '../components/SignupForm.vue'
 import LoginForm from '../components/LoginForm.vue'
+import { ref } from '@vue/reactivity'
 
 export default {
-  components: { SignupForm, LoginForm }
+  components: { SignupForm, LoginForm },
+  setup() {
+    const showLogin = ref(true)
+    
+    
+    
+    return { showLogin }
+  }
 }
 </script>
 
@@ -37,5 +53,13 @@ export default {
     outline: none;
     color: #999;
     margin: 10px auto;
+  }
+  .welcome span{
+    font-weight: bold;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  .welcome button {
+    margin: 20px auto;
   }
 </style>
