9c6e22cc275e68a24988af82645242c5a3361131
geostojanov <gestojanov@gmail.com>
2022-11-07 13:25:16 +0100

    Lesson 109 - setup context and custom events - Done

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -3,12 +3,12 @@
     <p>Welcome</p>
     <div v-if="showLogin">
       <h2>Login</h2>
-      <LoginForm/>
+      <LoginForm @login="enterChat"/>
       <p>No account yet? <span @click="showLogin = false">Signup</span></p>
     </div>
     <div v-else>
       <h2>Sign up</h2>
-      <SignupForm/>
+      <SignupForm @signup="enterChat"/>
       <p>Already have an account? <span @click="showLogin = true">Login</span></p>
     </div>
   </div>
@@ -18,15 +18,19 @@
 import SignupForm from '../components/SignupForm.vue'
 import LoginForm from '../components/LoginForm.vue'
 import { ref } from '@vue/reactivity'
+import { useRouter } from 'vue-router'
 
 export default {
   components: { SignupForm, LoginForm },
   setup() {
     const showLogin = ref(true)
+    const router = useRouter()
     
+    const enterChat = () => {
+      router.push({ name: 'Chatroom' })
+    }
     
-    
-    return { showLogin }
+    return { showLogin, enterChat }
   }
 }
 </script>
