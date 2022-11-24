a252876fbfd0a8b8a010ee36a18ad159d877d0a9
geostojanov <gestojanov@gmail.com>
2022-11-24 07:13:02 +0100

    Lesson 119 - creating the chat window - Done

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="container">
     <Navbar />
+    <ChatWindow />
     <NewChatForm />
   </div>
 </template>
 
 <script>
 import NewChatForm from '../components/NewChatForm.vue'
+import ChatWindow from '../components/ChatWindow.vue'
 import getUser from '@/composables/getUser'
 import Navbar from '../components/Navbar.vue'
 import { watch } from '@vue/runtime-core'
 import { useRouter } from 'vue-router'
 export default {
-  components: { Navbar, NewChatForm },
+  components: { Navbar, NewChatForm, ChatWindow },
   setup() {
     const { user } = getUser()
     const router = useRouter()
